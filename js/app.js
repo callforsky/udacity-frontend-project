d52b91d5474255c15dab433d19dd83dc133a60b5
chuanqin <callforsky@gmail.com>
2017-12-09 21:38:46 -0500

    Added Train Station List
    very basic form of train station list. This is a solid step in
    utilizing Knockout package. The next step is to make the filter button,
    return a value is enough for this step.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,34 @@
+// This is the octopus part
 var ViewModel = function() {
+	var self = this;
 
-}
+	this.trainStationList = ko.observableArray(allTrainStations);
+};
+
+
+// This is the data part
+var allTrainStations = [
+	{
+		name : 'White Plains',
+		line : 'Harlem Line',
+		address : {lat: 41.035453, lng: -73.774714}
+	},
+	{
+		name: 'North White Plains',
+		line : 'Harlem Line',
+		address: {lat: 41.051315, lng: -73.772475}
+	},
+	{
+		name: 'Cold Spring',
+		line: 'Hudson Line',
+		address: {lat: 41.415259, lng: -73.958101}
+	}
+];
+
+// This is the model part
+var trainStation = function(data) {
+	// var lineFlag = 
+};
+
 
 ko.applyBindings(new ViewModel())
